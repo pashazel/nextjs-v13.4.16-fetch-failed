fa1836ae4e99e72cb263c7957bda4c8f805f45e7
Pavel <pasha.zel@gmail.com>
2023-08-16 15:45:39 +0300

    first commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,7 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+async function handler(req, res) {
+  const data = await fetch( "https://reqres.in/api/users/2");
+  const result = await data.json();
+  res.status(200).json({ id: result.data });
 }
+
+export default handler;
